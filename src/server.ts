199ce3d3791b4dd51748a92ffde56462f223f762
Ronny Leibham <ronnyleibham@gmx.de>
2021-10-26 11:36:10 +0200

    Add 1. Aufgabe

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,17 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/api/users/:username', (_request, response) => {
+  response.send(_request.params.username);
+});
+
+app.get('/api/users', (_request, response) => {
+  const users = ['Homer', 'Lisa', 'Marge', 'Bart'];
+  response.send(users);
+});
+
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World 🐱‍👤!');
 });
 
 app.listen(port, () => {
