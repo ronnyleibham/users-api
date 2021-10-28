f82e3258db0ab700833fc19f9046c1509fedcc6d
Ronny Leibham <ronnyleibham@gmx.de>
2021-10-28 09:28:47 +0200

    Add Cookie Set

--- src/server.ts
+++ src/server.ts
@@ -1,56 +1,82 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 // Custom middleware to log requests
 app.use((request, _response, next) => {
   console.log('Request received', request.url);
   next();
 });
-
 // Middleware for parsing application/json
 app.use(express.json());
 
-const users = ['Homer', 'Bart', 'Marge', 'Lisa'];
+// Middleware for parsing cookies
+app.use(cookieParser());
 
-app.post('/api/users', (request, response) => {
-  const newUserName = request.body.name;
-  if (users.includes(newUserName)) {
-    response.status(409).send('User already exist. RTFM!');
+const users = [
+  {
+    name: 'Manuel',
+    username: 'manuel123',
+    password: '123abc',
+  },
+  {
+    name: 'Leon',
+    username: 'lmachens',
+    password: 'asdc',
+  },
+  {
+    name: 'Anke',
+    username: 'anke9000',
+    password: 'ab',
+  },
+  {
+    name: 'Philipp',
+    username: 'phgrtz',
+    password: 'pw123!',
+  },
+];
+
+app.get('/api/me', (request, response) => {
+  const username = request.cookies.username;
+  const foundUser = users.find((user) => user.username === username);
+  if (foundUser) {
+    response.send(foundUser);
   } else {
-    users.push(newUserName);
-    response.send(`${newUserName} added`);
+    response.status(404).send('User not found');
   }
 });
 
-app.delete('/api/users/:name', (request, response) => {
-  const usersIndex = users.indexOf(request.params.name);
-  if (usersIndex === -1) {
-    response.status(404).send("User doesn't exist. Check another Castle 🏰");
+app.post('/api/login', (request, response) => {
+  const credentials = request.body;
+  const existingUser = users.find(
+    (user) =>
+      user.username === credentials.username &&
+      user.password === credentials.password
+  );
+
+  if (existingUser) {
+    response.setHeader('Set-Cookie', `username=${existingUser.username}`);
+    response.send('Logged in');
+  } else {
+    response.status(401).send('You shall not pass');
+  }
+});
+app.post('/api/users', (request, response) => {
+  const newUser = request.body;
+  if (
+    typeof newUser.name !== 'string' ||
+    typeof newUser.username !== 'string' ||
+    typeof newUser.password !== 'string'
+  ) {
+    response.status(400).send('Missing properties');
     return;
   }
-
-  users.splice(usersIndex, 1);
-  response.send('Deleted');
-});
-
-app.get('/api/users/:name', (request, response) => {
-  const isNameKnow = users.includes(request.params.name);
-  if (isNameKnow) {
-    response.send(request.params.name);
+  if (users.some((user) => user.username === newUser.username)) {
+    response.status(409).send('User already exists');
   } else {
-    response.status(404).send('This page is not here. Check another Castle 🏰');
+    users.push(newUser);
+    response.send(`${newUser.name} added`);
   }
-});
-
-app.get('/api/users', (_request, response) => {
-  response.send(users);
-});
-
-app.get('/', (_req, res) => {
-  res.send('Hello World 🐱‍👤!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
